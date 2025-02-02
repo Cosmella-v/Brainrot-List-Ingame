f9af3c10a561ce989618568396a83ec052a5a80a
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2025-02-02 17:28:07 -0500

    refactor

--- src/Demon/LevelCell.cpp
+++ src/Demon/LevelCell.cpp
@@ -23,46 +23,27 @@ class $modify(LevelCell) {
 			BrType::parseRequestString(level_map);
 		}
 		LevelCell::loadFromLevel(p0);
-		CCNode* difficultyNode;
-		if (Loader::get()->isModLoaded("geode.node-ids")) {
-			difficultyNode = m_mainLayer->getChildByID("difficulty-container");
-			if (!difficultyNode) return;
-		}
-		CCNode* difficultySpr;
-		if (!Loader::get()->isModLoaded("geode.node-ids")) {
-			difficultyNode = nullptr;
-			for (auto item :  CCArrayExt<CCNode*>(m_mainLayer->getChildren())) {
-				if (GJDifficultySprite* spr = item->getChildByType<GJDifficultySprite>(0)) {
-					difficultySpr = spr;
-					difficultyNode = difficultySpr->getParent(); // great way without node ids
-					break;
-				}
-			}
-		} else difficultySpr = difficultyNode->getChildByID("difficulty-sprite");
+		CCNode* difficultyNode = m_mainLayer->getChildByID("difficulty-container");
+		if (!difficultyNode) return;
+		CCNode* difficultySpr = difficultyNode->getChildByID("difficulty-sprite");
 		if (!difficultySpr) return;
 
 		cocos2d::CCPoint difficultyPos = difficultySpr->getPosition() + CCPoint { .0f, .0f };
-		if (difficultyNode) {
-			auto demonpos = BrType::find(p0->m_levelID);
-			if (!demonpos) {
-				demonpos = BRPacks::find(p0->m_levelID);
+		auto demonpos = BrType::find(p0->m_levelID);
+		if (!demonpos) demonpos = BRPacks::find(p0->m_levelID);
+		if (!demonpos) return; // if it's STILL nullptr, abort
+		if (CCNode* moreDifficultiesSprite = this->getChildByIDRecursive("uproxide.more_difficulties/more-difficulties-spr")) {
+				moreDifficultiesSprite->setScale(0);
+				moreDifficultiesSprite->setVisible(false);
+				static_cast<CCSprite*>(moreDifficultiesSprite)->setOpacity(0);
 			}
-			if (demonpos) {
-				if (CCNode* Boo = this->getChildByIDRecursive("uproxide.more_difficulties/more-difficulties-spr")) {
-					Boo->setScale(0);
-					Boo->setVisible(false);
-					typeinfo_cast<CCSprite*>(Boo)->setOpacity(0);
-				}
-                CCSprite* mdSpr = (!Mod::get()->getSettingValue<bool>("better-face")) ? CCSprite::create("normal_face_with_demon_text.png"_spr) : CCSprite::create("Betterface.png"_spr);
-				mdSpr->setPosition(difficultyPos);
-				if (mdSpr->getParent() != difficultyNode) difficultyNode->addChild(mdSpr);
-				mdSpr->setZOrder(difficultySpr->getZOrder());
-				mdSpr->setScale((!Mod::get()->getSettingValue<bool>("better-face")) ? 0.2 : 0.4);
-				mdSpr->setAnchorPoint({0.5,0.350});
-				typeinfo_cast<CCSprite*>(difficultySpr)->setOpacity(0);
-				mdSpr->setID("brl-difficulty-sprite"_spr); // more descriptive and kebab case node ID
-			}
-
-		} 
+			CCSprite* mdSpr = (!Mod::get()->getSettingValue<bool>("better-face")) ? CCSprite::create("normal_face_with_demon_text.png"_spr) : CCSprite::create("Betterface.png"_spr);
+			mdSpr->setPosition(difficultyPos);
+			if (mdSpr->getParent() != difficultyNode) difficultyNode->addChild(mdSpr);
+			mdSpr->setZOrder(difficultySpr->getZOrder());
+			mdSpr->setScale((!Mod::get()->getSettingValue<bool>("better-face")) ? 0.2 : 0.4);
+			mdSpr->setAnchorPoint({0.5,0.350});
+			typeinfo_cast<CCSprite*>(difficultySpr)->setOpacity(0);
+			mdSpr->setID("brl-difficulty-sprite"_spr); // more descriptive and kebab case node ID
 	}
 }; 
